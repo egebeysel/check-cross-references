c9162f3cc003b8d419a36b49316a91293b99c9e8
Ege Beysel <ege.beysel@rwth-aachen.de>
2023-12-01 14:42:21 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,24 +1,74 @@
+/* eslint-disable eslint-comments/no-duplicate-disable */
+/* eslint-disable prettier/prettier */
+/* eslint-disable github/array-foreach */
+/* eslint-disable no-shadow */
+/* eslint-disable prettier/prettier */
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const github = require('@actions/github')
+const { Context } = require('@actions/github/lib/context')
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    // Get token from the user input
+    const token = core.getInput('token', { required: true })
+    // Get context and octokit instance
+    const context = new Context()
+    const srcPrUrl = context.payload.pull_request.html_url
+    const octokit = github.getOctokit(token)
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // Request the timeline
+    const timeline = await octokit.request(
+      `GET /repos/{owner}/{repo}/issues/{issue_number}/timeline`,
+      {
+        ...context.repo,
+        issue_number: context.issue.number,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }
+    )
+    // Filter the cross-references
+    const crossRefs = timeline.data.filter(
+      elem => elem.event === 'cross-referenced'
+    )
+    // Check every box (if existing) in each PR
+    crossRefs.forEach(async elem => {
+      const comments_url = elem.source.issue.comments_url
+      const issue_number = elem.source.issue.number
+      const repo_name = elem.source.issue.repository.name
+      const repo_owner = elem.source.issue.repository.owner.login
+      // Fetch all comments of the referencing PRs
+      if (!('pull_request' in elem.source.issue)) return
+      const comments = await octokit.request(`GET ${comments_url}`, {
+        owner: repo_owner,
+        repo: repo_name,
+        issue_number,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+      const regexpFrom = new RegExp(
+        `- \\[ \\](\\s+?.*?(${srcPrUrl}).*?)$`,
+        'gm'
+      )
+      const replaceString = `- [X]$1`
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+      comments.data.forEach(async comment => {
+        const commentBody = comment.body
+        const newCommentBody = commentBody.replaceAll(regexpFrom, replaceString)
+        console.log(commentBody)
+        await octokit.request(`PATCH ${comment.url}`, {
+          owner: repo_owner,
+          repo: repo_name,
+          comment_id: comment.id,
+          body: newCommentBody,
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+          }
+        })
+      })
+    })
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
